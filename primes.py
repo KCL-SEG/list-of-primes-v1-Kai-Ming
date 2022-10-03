d2711ff46951edd4c9da60b63238e28ce134e1dd
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-03 01:33:52 +0100

    First attempt at primes

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    pointer = 1
+    counter = 0
+    while counter < number_of_primes:
+        pointer += 1
+        is_prime = True
+        for num in list:
+            if pointer % num != 0:
+                is_prime = True
+            else:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(pointer)
+            counter += 1
+    print(list)
     return list
+
+#primes(3)
--- primes_test.py
+++ primes_test.py
@@ -18,3 +18,13 @@ def test_20_primes():
     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
                 53, 59, 61, 67, 71]
     assert actual == expected
+
+"""
+def test():
+    test_1_primes()
+    test_2_primes
+    test_20_primes()
+    
+
+test()
+"""
